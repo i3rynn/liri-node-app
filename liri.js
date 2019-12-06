e744120ba599bb0ddfde2745a4211564fe0bbd57
Brynn Chapman <brynn_m_chapman@outlook.com>
2019-12-06 00:54:11 -0600

    Updated files.

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-//Add the code required to import the keys.js file and store it in a variable.
 var keys = require("./keys");
 var axios = require("axios");
 var moment = require("moment");
@@ -9,17 +8,8 @@ var spotify = new Spotify({
   id: keys.spotify.id,
   secret: keys.spotify.secret,
 });
-// var defaultSong = require("The Sign");
+
 var defaultMovie = "Mr. Nobody";
-// var spotify = new Spotify(keys.spotify);
-
-
-
-/**
- * Name of the venue
-Venue location
-Date of the Event (use moment to format this as "MM/DD/YYYY")
- */
 var action = process.argv[2];
 var value = process.argv[3];
 
@@ -28,14 +18,11 @@ switch (action) {
     getBands(value)
     break;
   case "spotify-this-song":
-    //If user has not specified a song , use default
-    // if (value === "") {
-    //   value = defaultSong;
-    // }
+    //Default song should display if user does not input one.
     getSongs(value)
     break;
   case "movie-this":
-    //If user has not specified a movie , use default
+    //Default movie should display if user does not input one.
     if (value == "") {
       value = defaultMovie;
     }
@@ -48,7 +35,6 @@ switch (action) {
     break;
 }
 function getBands(artist) {
-  // var artist = value;
   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
     .then(function (response) {
       console.log("Name of the venue:", response.data[0].venue.name);
@@ -62,9 +48,7 @@ function getBands(artist) {
 }
 
 function getSongs(songName) {
-  // var songName = value;
-
-  //If user has not specified a song , default to "The Sign" by Ace of Bass
+ 
   if (songName === "") {
     songName = "I Saw the Sign";
   }
@@ -73,27 +57,16 @@ function getSongs(songName) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    // else {
-    //   console.log("Not right now. Later?")
 
-    // console.log(JSON.stringify(data)); 
-
-    // The song's name
-
-    //Artist(s)
     console.log("Artists: ", data.tracks.items[0].album.artists[0].name)
-    // A preview link of the song from Spotify
     console.log("Preview Link: ", data.tracks.items[0].preview_url)
-    // The album that the song is from
     console.log("Album Name: ", data.tracks.items[0].album.name)
   });
 }
 
 function getMovies(movieName) {
-  // var movieName = value;
   axios.get("http://www.omdbapi.com/?apikey=42518777&t=" + movieName)
     .then(function (data) {
-      // console.log(data.data); 
       var results = `
       Title of the movie: ${data.data.Title}
       Year the movie came out: ${data.data.Year}
@@ -104,17 +77,11 @@ function getMovies(movieName) {
       Plot of the movie: ${data.data.Plot}
       Actors in the movie: ${data.data.Actors}`;
       console.log(results)
-
-      // console.log(data);
-      // console.log("Name of the venue:", response.data[0].venue.name);
-      // console.log("Venue location:", response.data[0].venue.city);
-      // var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
-      // console.log("Date of the Event:", eventDate);
     })
     .catch(function (error) {
       console.log(error);
     });
-    //Response if user does not type in a movie title
+    //Mr. Nobody will display if user does not enter a title.
     if (movieName === "Mr. Nobody") {
       console.log("-----------------------");
       console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
